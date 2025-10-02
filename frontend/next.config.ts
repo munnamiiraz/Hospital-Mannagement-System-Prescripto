b2af337c25e403593a1ded000c0d6b426a36b567
munnamiiraz <munnamiiraz@gmail.com>
2025-10-03 00:42:24 +0600

    refactor(next.config.ts): clean up configuration and add eslint settings

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,14 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["res.cloudinary.com"], // Cloudinary images allowed
+    domains: ["res.cloudinary.com"], 
   },
-  webpack(config) {
-    // SVGs imported as URLs so next/image can use them
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
-      type: "asset/resource", // gives URL string for SVGs
+      type: "asset/resource",
     });
     return config;
   },
